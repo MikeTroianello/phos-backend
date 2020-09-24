8026854720070fe177c71dfb841f6d7721acc073
Mike <troianello@protonmail.com>
2020-09-24 16:56:49 -0500

    cors

--- app.js
+++ app.js
@@ -50,18 +50,18 @@ mongoose
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 
-app.use(
-  cors({
-    credentials: true,
-    origin: ['http://phos.surge.sh'],
-  })
-);
 // app.use(
 //   cors({
 //     credentials: true,
-//     origin: ['http://localhost:3000']
+//     origin: ['http://phos.surge.sh'],
 //   })
 // );
+app.use(
+  cors({
+    credentials: true,
+    origin: ['http://localhost:3000'],
+  })
+);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
