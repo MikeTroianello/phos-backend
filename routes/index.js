8555d530a3913004dc0d427ed3fcae24206d0775
Mike <troianello@protonmail.com>
2020-09-24 16:17:23 -0500

    heroku test

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
   console.log("IT'S BEEN GOT");
-  res.json({ message: 'SUCCESS' });
+  res.json({ message: 'Welcome to the Phos Backend' });
   // res.render('index', { title: 'Express' });
 });
 
